9dc356667f675712416bac6389b4e134eb81dc4f
Luka <luka.baotian@gmail.com>
2018-04-21 20:06:51 +0200

    add firefox test

--- src/v1/FireFoxTest.java
+++ src/v1/FireFoxTest.java
@@ -1,10 +1,19 @@
 package v1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class FireFoxTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Luka\\Desktop\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		
+//		String Url = "localhost.com";
+		driver.get("https://www.google.com/");
+		
+driver.close();
 	}
 
 }
